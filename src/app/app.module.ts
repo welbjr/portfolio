14952bfd83e856e557768d7223c492ed86976fca
Welb Ćésar <cesarwelb@gmail.com>
2022-10-07 09:28:33 -0300

    Currículo pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { BannerComponent } from './pages/landing-page/banner/banner.component';
+import { ResumeComponent } from './pages/landing-page/resume/resume.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    BannerComponent
+    BannerComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +28,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     // Material
     MatGridListModule,
     MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
